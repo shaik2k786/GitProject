dc391c9bbb596bfdb47a2ae7bfc6a86e5daf3d3f
Saidashaik786 <108187239+Saidashaik786@users.noreply.github.com>
2023-06-06 11:46:54 +0530

    git eclipse test

---
+++ src/com/shaik/Demo.java
@@ -0,0 +1,15 @@
+package com.shaik;
+
+public class Demo {
+	
+	public void add() {
+		System.out.println("Add method");
+	}
+	public static void main(String[] args) {
+      
+		Demo d=new Demo();
+				
+				d.add();
+				
+	}
+}
